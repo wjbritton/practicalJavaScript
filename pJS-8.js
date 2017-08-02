f4f0be3085069f388b723312425c1b50f3517319
wjbritton <wjbritton1@gmail.com>
2017-08-02 14:58:27 +0000

    Commiting pJS-8

---
+++ pJS-8.js
@@ -0,0 +1,78 @@
+let todoList = {
+    todos: [],
+    displayTodos: function() {
+        if (this.todos.length === 0) {
+            console.log('Your todo list is empty!')
+        } else {
+            console.log('My Todos:')
+            for(let i = 0; i < this.todos.length; i++) {
+                if(this.todos[i].completed === true) {
+                    console.log('(x)', this.todos[i].todoText);
+                } else {
+                    console.log('()', this.todos[i].todoText)
+                }
+            }
+        }
+    },
+    addTodo: function(todoText) {
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        })
+        this.displayTodos()
+    },
+    toggleAll: function() {
+        let totalTodos = this.todos.length
+        let completedTodos = 0
+    for(let i = 0; i< totalTodos;i++){
+            if(this.todos[i].completed === true){
+                completedTodos++
+            }
+        }
+        
+        
+        if(completedTodos === totalTodos) {
+            for(let i = 0; i< totalTodos;i++){
+                    this.todos[i].completed = false
+                
+                }
+            } else {
+                for( let i = 0; i < totalTodos; i++){
+                    this.todos[i].completed = true
+                }
+            }
+        this.displayTodos()
+    }
+}
+
+    let handlers = {
+        displayTodos: function() {
+        todoList.displayTodos()
+    },
+        toggleAll: function() {
+        todoList.toggleAll()
+    },
+    addTodo: function() {
+        let addTodoTextInput = document.getElementById('addTodoTextInput')
+        todoList.addTodo(addTodoTextInput.value)
+        addTodoTextInput.value = ''
+    },
+    changeTodo: function() {
+        let changeTodoPositionInput = document.getElementById('changeTodosPositionInput')
+        let changeTodoTextInput = document.getElementById('changeTodoTextInput')
+        todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value)
+        changeTodoPositionInput.value = ''
+        changeTodoTextInput.value = ''
+    },
+    deleteTodo: function() {
+        let deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput')
+        todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber)
+        deleteTodoPositionInput.value = ''
+    },
+    toggleCompleted: function() {
+        let toggleCompletedPositionInput = document.getElementById('toggleCompleted')
+        todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber)
+        toggleCompletedPositionInput.value = ''
+    }
+        
+}
